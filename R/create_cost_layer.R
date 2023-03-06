ed6f45c6f5efe6bacfb6c5c45843f89e5d94e832
gcperk <genevieve.c.perkins@gmail.com>
2023-03-06 13:49:01 -0800

    update create cost to output transition layer

--- R/create_cost_layer.R
+++ R/create_cost_layer.R
@@ -2,18 +2,17 @@
 #'
 #' @param x **SpatRast or RasterLayer (raster package), output from prepare_cost_layer ()
 #' @param start **SpatVec of sf object with Start Locations for the cost layer.
+#' @param trans_output TRUE/FALSE - do you want to save output of transition layer as rds (to be used in vrp)
+#' @param transition_outdir **filepath for where to output transition rds
 #' @import magrittr
 #' @import gdistance
-#' @return RasterLayer with Cost
+#' @return RasterLayer with Cost and output the transition file for Vrp use
 #' @export
 #'
 #' @examples
-#' create_cost(costprep, start)
+#' create_cost(costprep, start, outdir)
 
-#x <- costprep
-#start <- helidrop
-
-create_cost_layer <- function(x, start){
+create_cost_layer <- function(x, start, trans_output = FALSE, transition_outdir = NULL){
 
   # if is spatRast need to convert first
 
@@ -35,8 +34,12 @@ create_cost_layer <- function(x, start){
   # create  transition layer
   tr <- gdistance::transition(x, transitionFunction = function(x) 1/mean(x,na.rm = T), directions = 8, symm = F)
   tr1 <- gdistance::geoCorrection(tr)
-
   rm(tr)
+
+  if(trans_output == TRUE){
+  saveRDS(tr1, file.path(transition_outdir,"transition_layer.rds"))
+  }
+
   acost <- gdistance::accCost(tr1,start)
   gc()
   terra::plot(acost)
