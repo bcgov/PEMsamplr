4ea99a4b4b2ea8bbc385a2c28849e7bf7391e003
gcperk <genevieve.c.perkins@gmail.com>
2023-03-16 08:51:00 -0700

    update documentation

--- R/prep_cost_layer_LCP.R
+++ R/prep_cost_layer_LCP.R
@@ -3,7 +3,7 @@
 #' @param x x \code{SpatRaster} Digital Elevation Model (DEM)
 #' @param cost_function cost_function \code{character} or \code{function}. Cost function applied to slope values. See details for implemented cost functions. tobler (default)
 #' @param neighbours \code{numeric} value. Number of directions used in the conductance matrix calculation. Expected numeric values are 4, 8, 16, 32, 48, or matrix object. 16 (default)
-#' @param roads
+#' @param roads \code{sf} of road network
 #' @param crit_slope \code{numeric} value. Critical Slope (in percentage) is 'the transition where switchbacks become more effective than direct uphill or downhill paths'. Cost of climbing the critical slope is twice as high as those for moving on flat terrain and is used for estimating the cost of using wheeled vehicles. Default value is 12, which is the postulated maximum gradient traversable by ancient transport (Verhagen and Jeneson, 2012). Critical slope only used in 'wheeled transport' cost function
 #' @param max_slope \code{numeric} value. Maximum percentage slope that is traversable. Slope values that are greater than the specified max_slope are given a conductivity value of 0. If cost_function argument is 'campbell 2019' or 'campbell' then max_slope is fixed at 30 degrees slope to reflect the maximum slope that the cost function is parametised to. NULL (default)
 #' @param percentile   \code{numeric} value. Travel rate percentile only used in 'campbell 2019' cost_function. Expected numeric values are 0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.99. 0.5 (default)
@@ -21,7 +21,7 @@
 #' cost_function = function(x) {(6 * exp(-3.5 * abs(x + 0.05))) / 3.6}, neighbours = 4)
 #' loc <- sf::st_sf(geometry = sf::st_sfc(sf::st_point(c(839769, 4199443)),crs = terra::crs(r)))
 #' r <- terra::rast(system.file("extdata/SICILY_1000m.tif", package="leastcostpath"))
-#' slope_cs <- create_slope_cs(x = r, cost_function = "tobler", neighbours = 4)
+#' slope_cs <- create_slope_cs(x = r, roads = roads, cost_function = "tobler", neighbours = 4)
 #' cc <- create_accum_cost(x = slope_cs, origin = loc, rescale = TRUE)
 
 
