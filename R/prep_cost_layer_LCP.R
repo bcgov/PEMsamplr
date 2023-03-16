dc11d87e6494bc5a5b0f6f94c6177e261348cb20
gcperk <genevieve.c.perkins@gmail.com>
2023-03-16 08:45:41 -0700

    add clhs cost function based on terra

---
+++ R/prep_cost_layer_LCP.R
@@ -0,0 +1,203 @@
+#' Create Cost Layer from Slope
+#'
+#' @param x x \code{SpatRaster} Digital Elevation Model (DEM)
+#' @param cost_function cost_function \code{character} or \code{function}. Cost function applied to slope values. See details for implemented cost functions. tobler (default)
+#' @param neighbours \code{numeric} value. Number of directions used in the conductance matrix calculation. Expected numeric values are 4, 8, 16, 32, 48, or matrix object. 16 (default)
+#' @param roads
+#' @param crit_slope \code{numeric} value. Critical Slope (in percentage) is 'the transition where switchbacks become more effective than direct uphill or downhill paths'. Cost of climbing the critical slope is twice as high as those for moving on flat terrain and is used for estimating the cost of using wheeled vehicles. Default value is 12, which is the postulated maximum gradient traversable by ancient transport (Verhagen and Jeneson, 2012). Critical slope only used in 'wheeled transport' cost function
+#' @param max_slope \code{numeric} value. Maximum percentage slope that is traversable. Slope values that are greater than the specified max_slope are given a conductivity value of 0. If cost_function argument is 'campbell 2019' or 'campbell' then max_slope is fixed at 30 degrees slope to reflect the maximum slope that the cost function is parametised to. NULL (default)
+#' @param percentile   \code{numeric} value. Travel rate percentile only used in 'campbell 2019' cost_function. Expected numeric values are 0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.99. 0.5 (default)
+#' @param exaggeration \code{logical}. if TRUE, positive slope values (up-hill movement) multiplied by 1.99 and negative slope values (down-hill movement) multiplied by 2.31
+#' @param exaggeration  if TRUE, positive slope values (up-hill movement) multiplied by 1.99 and negative slope values (down-hill movement) multiplied by 2.31
+#' @author  Joseph Lewis (https://github.com/josephlewis/leastcostpath)
+#' @return \code{conductanceMatrix} that numerically expresses the difficulty of moving across slope based on the provided cost function
+#' @export
+#'
+#' @examples
+#' r <- terra::rast(system.file("extdata/SICILY_1000m.tif", package="leastcostpath"))
+#'
+#' slope_cs <- create_slope_cs(x = r, cost_function = "tobler", neighbours = 4)
+#' slope_cs2 <- create_slope_cs(x = r,
+#' cost_function = function(x) {(6 * exp(-3.5 * abs(x + 0.05))) / 3.6}, neighbours = 4)
+#' loc <- sf::st_sf(geometry = sf::st_sfc(sf::st_point(c(839769, 4199443)),crs = terra::crs(r)))
+#' r <- terra::rast(system.file("extdata/SICILY_1000m.tif", package="leastcostpath"))
+#' slope_cs <- create_slope_cs(x = r, cost_function = "tobler", neighbours = 4)
+#' cc <- create_accum_cost(x = slope_cs, origin = loc, rescale = TRUE)
+
+
+
+library(devtools)
+install_github("josephlewis/leastcostpath")
+library(leastcostpath)
+
+# need several functions from this
+source("R/prep_cost_layer_LCP_utils.R")
+# need several functions from this
+#
+# x = terra::rast(file.path("D:/PEM_DATA/DateCreek_AOI/DateCreek_AOI/10_clean_inputs/20_covariates", "25m", "dem.tif"))
+# origin <- sf::st_sf(geometry = sf::st_sfc(sf::st_point(c(892344.7, 1154340)),crs = terra::crs(x)))
+# vec_dir <- "D:\\PEM_DATA\\DateCreek_AOI\\DateCreek_AOI\\10_clean_inputs\\10_vector\\"
+#   ## prepare roads layer
+#   roads <- sf::st_read(file.path(vec_dir, "road_major.gpkg"), quiet = TRUE) %>%
+#     sf::st_zm()
+#
+
+
+create_slope_cs <- function(x, cost_function = "tobler offpath", neighbours = 16, roads, crit_slope = 12, max_slope = NULL, percentile = 0.5, exaggeration = FALSE) {
+
+  # x = terra::rast(file.path("D:/PEM_DATA/DateCreek_AOI/DateCreek_AOI/10_clean_inputs/20_covariates", "25m", "dem.tif"))
+  # neighbours = 8
+  # cost_function = "tobler offpath"
+  # crit_slope = 12
+  # max_slope = 40
+  # percentile = 0.5
+  # exaggeration = FALSE
+
+
+  if(terra::is.lonlat(x)) {
+    stop("supplied digital elevation model (DEM) is invalid. x argument expects DEM with a projected coordinate system")
+  }
+
+
+  neighbours <- neighbourhood(neighbours = neighbours)
+
+  # prepare roads layer
+  roads$ROAD_CLASS[roads$trail == 1] <- "trail"
+  roads <- roads[,c("ROAD_SURFACE","ROAD_CLASS", "ROAD_NAME_FULL")]
+  roads <- roads %>% dplyr::rename('road_surface' = ROAD_CLASS,
+                                   'surface' = ROAD_SURFACE,
+                                   'name' = ROAD_NAME_FULL)
+  rdsAll <-  data.table::as.data.table(roads) %>% sf::st_as_sf()
+  rSpd <- dplyr::tibble(
+    "road_surface" = c("resource", "unclassified", "recreation", "trail", "local", "collector", "highway", "service", "arterial", "freeway", "strata", "lane", "private", "yield", "ramp", "restricted", "water", "ferry", "driveway","unclassifed"),
+    "speed" = c(30, 30, 50, 4.5, 50, 80, 80, 50, 80, 80, 30, 30, 4.5, 30, 60, 4.5, 0.1, 0.1, 4.5, 30))
+  #"speed" = c(3000, 3000, 5000, 4.5, 5000, 8000, 8000, 50, 8000, 8000, 3000, 3000, 4.5, 3000, 6000, 4.5, 0.1, 3000, 4.5, 3000))
+
+  #   # convert speed to pace
+  rSpd <- data.table::as.data.table(rSpd)
+
+  rdsAll <- merge(rdsAll, rSpd, by = "road_surface", all = F)
+  rdsAll <- rdsAll[,"speed"]
+
+  #   # create a roads raster (buffered)
+  rdsAll <- sf::st_buffer(rdsAll, dist = 25, endCapStyle = "SQUARE", joinStyle = "MITRE")
+  rdsAll <- sf::st_cast(rdsAll, "MULTIPOLYGON")
+  rdsRast <- terra::rasterize(rdsAll, x, field = "speed", fun = "max")
+  rdsRast[is.nan(rdsRast[])] <- NA
+
+  # identify cells and which are NA
+  road_cells <- which(!is.na(terra::values(rdsRast)))
+  not_road_cells <- which(is.na(terra::values(rdsRast)))
+
+  # get cells that are adjacent
+  radj <- terra::adjacent(x = rdsRast, cells = cells, directions = neighbours, pairs = TRUE)
+  # remove any that are na
+  radj <- radj[!radj[,2] %in% na_cells,]
+
+  # get the actual values
+  road_values <- terra::values(rdsRast)[,1]
+
+  #Calculate the rise/run
+  rspeed <- road_values[radj[,1]]
+
+
+  # prepare slope component
+
+  # identify cells and which are NA
+  cells <- which(!is.na(terra::values(x)))
+  na_cells <- which(is.na(terra::values(x)))
+
+  # get cells that are adjacent
+  adj <- terra::adjacent(x = x, cells = cells, directions = neighbours, pairs = TRUE)
+  # remove any that are na
+  adj <- adj[!adj[,2] %in% na_cells,]
+
+  # get the actual values
+  elev_values <- terra::values(x)[,1]
+
+  message("calculating slope...")
+
+  #Calculate the rise/run
+  rise <- (elev_values[adj[,2]] - elev_values[adj[,1]])
+  # calculate the distance from each the adjacent pixal
+  run <- calculate_distance(x = x, adj = adj)
+
+  mathematical_slope <- rise/run
+
+  if(exaggeration) {
+    mathematical_slope <- ifelse(mathematical_slope > 0, mathematical_slope * 1.99, mathematical_slope * 2.31)
+  }
+
+  ncells <- length(cells) + length(na_cells)
+
+  # selectd which cost function to use
+  cf <- cost(cost_function = cost_function, crit_slope = crit_slope, percentile = percentile)
+
+  if(is.function(cost_function)) {
+    message(c("Applying ", deparse(body(cost_function)[[2]]), " cost function"))
+  } else{
+    message(c("Applying ", cost_function, " cost function"))
+  }
+
+  speed <- cf(mathematical_slope)
+
+  # update speed for pixals where there is a road
+
+  rspeed[is.na(rspeed)] <- speed
+
+  speed <- rspeed  #speed (km/h)
+
+  conductance <- speed/run
+
+  if(!is.function(cost_function)) {
+    if(cost_function %in% c("campbell 2019", "campbell")) {
+      # convert 30 degrees slope to percentage slope
+      max_slope <- tan(30*pi/180)*100
+    }
+  }
+
+  if(!is.null(max_slope)) {
+    # convert percentage max slope to mathematical max slope
+    # might consider putting in a steepness % of 15% as max?
+
+    max_slope <- max_slope/100
+    index <- abs(mathematical_slope) >= max_slope
+    conductance[index] <- 0
+  }
+
+  cs_matrix <- Matrix::Matrix(data = 0, nrow = ncells, ncol = ncells, sparse = TRUE)
+  cs_matrix[adj] <- conductance
+
+  cs <- list("conductanceMatrix" = cs_matrix,
+             "costFunction" = cost_function,
+             "maxSlope" = ifelse(!is.null(max_slope), paste0(max_slope*100, "%"), NA),
+             "exaggeration" = exaggeration,
+             "criticalSlope" = ifelse(test = !is.function(cost_function), yes = ifelse(test = cost_function == "wheeled transport", yes = paste0(max_slope, "%"), no = NA), no = NA),
+             "percentile" = ifelse(test = !is.function(cost_function), yes = ifelse(test = cost_function == "campbell 2019", yes = percentile, no = NA), no = NA),
+             "neighbours" = sum(neighbours, na.rm = TRUE),
+             "resolution" = terra::res(x),
+             "nrow" = terra::nrow(x),
+             "ncol" = terra::ncol(x),
+             "extent" = x@ptr$extent$vector,
+             "crs" = terra::crs(x, proj = TRUE))
+
+  class(cs) <- "conductanceMatrix"
+
+  return(cs)
+
+}
+
+
+#
+#
+# create_slope_cs(x, cost_function = "tobler offpath",
+#                 neighbours = 8,
+#                 roads = roads,
+#                 crit_slope = 12,
+#                 max_slope = 50,
+#                 percentile = 0.5,
+#                 exaggeration = FALSE)
+#
+#
+# acost <- create_accum_cost(cs, origin, rescale = FALSE)
+# terra::plot(acost)
---
+++ R/prep_cost_layer_LCP_utils.R
@@ -0,0 +1,276 @@
+#' @noRd
+#' These scripts provide various utility for the cost layer creation. These are all
+#' were written by Joseph Lewis under the leastcostpath package V.2
+#' They are reproduced here as they are not currently available outside the package
+#' https://github.com/josephlewis/leastcostpath/blob/master/R/neighbourhood.R
+
+neighbourhood <- function(neighbours) {
+
+  neighbours_32 <- matrix(c(0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1,
+                            1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0), nrow = 7, ncol = 7, byrow = TRUE)
+
+  neighbours_48 <- matrix(c(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1,
+                            1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0,
+                            1, 0, 1, 0, 1, 0, 1, 0, 1, 0), nrow = 9, ncol = 9, byrow = TRUE)
+
+  if (inherits(neighbours, "matrix")) {
+    neighbours <- neighbours
+  } else if (neighbours == 4) {
+    neighbours <- 4
+  } else if (neighbours == 8) {
+    neighbours <- 8
+  } else if (neighbours == 16) {
+    neighbours <- 16
+  } else if (neighbours == 32) {
+    neighbours <- neighbours_32
+  } else if (neighbours == 48) {
+    neighbours <- neighbours_48
+  } else (
+    stop(paste0("neighbours argument invalid. Expecting 4, 8, 16, 32, 48, or a matrix object"))
+  )
+
+  return(neighbours)
+}
+
+
+
+calculate_distance <- function(x, adj) {
+
+  xy1 <- terra::xyFromCell(x, adj[, 1])
+  xy2 <- terra::xyFromCell(x,adj[, 2])
+
+  xy3 <- (xy1[,1] - xy2[,1])^2
+  xy4 <- (xy1[,2] - xy2[,2])^2
+
+  dist <- sqrt(xy3 + xy4)
+
+  return(dist)
+
+}
+
+
+cost <- function(cost_function, crit_slope, percentile) {
+
+  cfs <- c("tobler", "tobler offpath", "davey", 'rees', "irmischer-clarke male", "irmischer-clarke offpath male", "irmischer-clarke female", "irmischer-clarke offpath female", "modified tobler", 'garmy', 'kondo-saino', "wheeled transport", "herzog", "llobera-sluckin", "naismith", "minetti", "campbell", "campbell 2019", "sullivan")
+
+  if (inherits(cost_function, "character")) {
+    if (!cost_function %in% cfs) {
+      stop("cost_function argument is invalid. See details for accepted cost functions")
+    }
+
+    # mathematical slope to degrees
+    slope2deg <- function(slope) { (atan(slope) * 180/pi)}
+
+    # degrees to radians
+    deg2rad <- function(deg) {(deg * pi) / (180)}
+
+    # Tobler Hiking Function measured in km/h. Divide by 3.6 to turn into m/s
+    if (cost_function == "tobler") {
+
+      # 3.6 converts from km/h to m/s
+      cf <- function(x) {
+        (6 * exp(-3.5 * abs(x + 0.05))) / 3.6
+      }
+    }
+
+    if (cost_function == "tobler offpath") {
+
+      cf <- function(x) {
+        ((6 * exp(-3.5 * abs(x + 0.05))) * 0.6) / 3.6
+      }
+
+    }
+
+    # requires mathematical slope to be in radians. Necessary to convert mathematical slope to degrees and then to radians
+    if (cost_function == "davey") {
+
+      cf <- function(x, y = 1.40, z = 2.8) {
+        x_deg <- slope2deg(x)
+        x_rad <- deg2rad(x_deg)
+
+        (y * exp(-z*abs(x_rad)))
+      }
+
+    }
+
+    if (cost_function == "rees") {
+
+      cf <- function(x) {
+        (1 / (0.75 + 0.09 * abs(x) + 14.6 * (abs(x))^2))
+      }
+
+    }
+
+    if (cost_function == "irmischer-clarke male") {
+
+      cf <- function(x) {
+        (0.11 + exp(-(abs(x) * 100 + 5)^2/(2 * 30^2)))
+
+      }
+
+    }
+
+    if (cost_function == "irmischer-clarke offpath male") {
+
+      cf <- function(x) {
+        (0.11 + 0.67 * exp(-(abs(x) * 100 + 2)^2/(2 * 30^2)))
+      }
+
+    }
+
+    if (cost_function == "irmischer-clarke female") {
+
+      cf <- function(x) {
+        (0.95 * (0.11 + exp(-(abs(x) * 100 + 5)^2/(2 * 30^2))))
+      }
+
+    }
+
+    if (cost_function == "irmischer-clarke offpath female") {
+
+      cf <- function(x) {
+        0.95 * (0.11 + 0.67 * exp(-(abs(x) * 100 + 2)^2/(2 * 30^2)))
+      }
+
+    }
+
+    if (cost_function == "modified tobler") {
+
+      cf <- function(x) {
+        (4.8 * exp(-5.3 * abs((x * 0.7) + 0.03)))
+      }
+
+    }
+
+    if (cost_function == "garmy") {
+
+      cf <- function(x) {
+        (4 * exp(-0.008 * (slope2deg(x)^2)))
+      }
+
+    }
+
+    if (cost_function == "kondo-saino") {
+
+      cf <- function(x) {
+        ifelse(abs(x) >= -0.07, (5.1 * exp(-2.25 * abs(x + 0.07))), (5.1 * exp(-1.5 * abs(x + 0.07))))
+      }
+
+    }
+
+    if (cost_function == "wheeled transport") {
+
+      cf <- function(x) {
+        (1/(1 + ((abs(x) * 100)/crit_slope)^2))
+      }
+
+    }
+
+    if (cost_function == "herzog") {
+
+      cf <- function(x) {
+
+        (1/((1337.8 * (x)^6) + (278.19 * (x)^5) - (517.39 * (x)^4) - (78.199 * (x)^3) + (93.419 * (x)^2) + (19.825 * (x)) + 1.64))
+
+      }
+
+    }
+
+    if (cost_function == "llobera-sluckin") {
+
+      cf <- function(x) {
+        (1/(2.635 + (17.37 * (x)) + (42.37 * (x)^2) - (21.43 * (x)^3) + (14.93 * (x)^4)))
+      }
+
+    }
+
+    if (cost_function == "naismith") {
+
+      cf <- function(x) {
+
+        x_deg <- slope2deg(x)
+        x_rad <- deg2rad(x_deg)
+
+        ifelse(x_deg > 0, (1 / (0.72 + 6 * tan(x_rad))), ifelse(x_deg <= -12, (1 / (0.72 - 2 * tan(x_rad))), ifelse(x_deg > -12 & x_deg <= -5, (1 / (0.72 + 2 * tan(x_rad))), 1.40)))
+      }
+    }
+
+    if (cost_function == "minetti") {
+
+      cf <- function(x) {
+        (1/((280.5 * abs(x)^5) - (58.7 * abs(x)^4) - (76.8 * abs(x)^3) + (51.9 * abs(x)^2) + (19.6 * abs(x)) + 2.5))
+      }
+    }
+
+    if (cost_function == "campbell") {
+
+      cf <- function(x) {
+        x_deg <- slope2deg(x)
+
+        1.662 - (5.191 * 10^-3) * x_deg - (1.127*10^-3) * x_deg^2
+      }
+
+    }
+
+    if (cost_function == "campbell 2019") {
+
+      percentile_choice <- c(0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9,
+                             0.95, 0.99)
+
+      if (!percentile %in% percentile_choice) {
+        stop("percentile argument is invalid. Expecting percentile value of 0.01, 0.05, 0.10, 0.15, 0.20,
+        0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95 or 0.99")
+      }
+
+      term_a <- c(-2.1, -1.527, -1.568, -1.626, -1.71, -1.822, -1.858, -1.891, -1.958, -2.05, -2.171, -2.317, -2.459, -2.647, -2.823,
+                  -3.067, -3.371, -3.661, -3.06, -3.485, -4)
+      term_b <- c(12.273, 14.041, 13.328, 11.847, 10.154, 8.827, 8.412, 8.584, 8.96, 9.402, 10.064, 10.712, 11.311, 12.089, 12.784,
+                  13.888, 15.395, 17.137, 16.653, 17.033, 13.903)
+      term_c = c(21.816, 36.813, 38.892, 38.231, 36.905, 37.111, 39.995, 44.852, 50.34, 56.172, 63.66, 71.572, 79.287, 89.143, 98.697,
+                 113.655, 134.409, 159.027, 138.875, 138.04, 123.515)
+      term_d = c(0.263, 0.32, 0.404, 0.481, 0.557, 0.616, 0.645, 0.649, 0.649, 0.646, 0.628, 0.608, 0.599, 0.576, 0.566, 0.518, 0.443,
+                 0.385, 0.823, 1.179, 1.961)
+      term_e = c(-0.00193, -0.00273, -0.00323, -0.00356, -0.00389, -0.00402, -0.0043, -0.00443, -0.00457, -0.0046, -0.00463, -0.00451,
+                 -0.00461, -0.00465, -0.00493, -0.00488, -0.00472, -0.00534, -0.01386, -0.01252, -0.01081)
+
+      lorentz_function_terms <- data.frame(percentile = percentile_choice, term_a, term_b, term_c, term_d, term_e)
+      terms <- lorentz_function_terms[lorentz_function_terms$percentile == percentile, ]
+
+      cf <- function(x){
+        (terms$term_c*(1/(pi*terms$term_b*(1+((slope2deg(x)-terms$term_a)/terms$term_b)^2)))+terms$term_d)+terms$term_e*slope2deg(x)
+      }
+    }
+
+    if (cost_function == "sullivan") {
+
+      percentile_choice <- c(0.167, 0.5, 0.833)
+
+      if (!percentile %in% percentile_choice) {
+        stop("percentile argument is invalid. Expecting percentile value of 0.167, 0.5, 0.833")
+      }
+
+      term_a <- c(-3.3717, -2.8292, -2.2893)
+      term_b <- c(25.8255, 20.9482, 19.4024)
+      term_c <- c(92.6594, 77.6346, 65.3577)
+      term_d <- c(-0.1624, 0.2228, 0.6226)
+      term_e <- c(0.0019, -0.0004, -0.0020)
+
+      lorentz_function_terms <- data.frame(percentile = percentile_choice, term_a, term_b, term_c, term_d, term_e)
+      terms <- lorentz_function_terms[lorentz_function_terms$percentile == percentile, ]
+
+      cf <- function(x){
+        (terms$term_c*(1/(pi*terms$term_b*(1+((slope2deg(x)-terms$term_a)/terms$term_b)^2)))+terms$term_d)+terms$term_e*slope2deg(x)
+      }
+
+    }
+
+  }
+
+  if(is.function(cost_function)) {
+
+    cf <- cost_function
+
+  }
+
+  return(cf)
+}
