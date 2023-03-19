bce04e4ef0530c4695d0be17369e21ef25bf366e
gcperk <genevieve.c.perkins@gmail.com>
2023-03-19 09:51:45 -0700

    switch to km'h

--- R/prep_cost_layers.R
+++ R/prep_cost_layers.R
@@ -31,8 +31,8 @@ prep_cost_layers <- function(vec_dir, dem, heli = FALSE) {
     rdsAll <-  data.table::as.data.table(roads) %>% sf::st_as_sf()
     rSpd <- dplyr::tibble(
       "road_surface" = c("resource", "unclassified", "recreation", "trail", "local", "collector", "highway", "service", "arterial", "freeway", "strata", "lane", "private", "yield", "ramp", "restricted", "water", "ferry", "driveway","unclassifed"),
-      #"speed" = c(30, 30, 50, 4.5, 50, 80, 80, 50, 80, 80, 30, 30, 4.5, 30, 60, 4.5, 0.1, 0.1))
-      "speed" = c(3000, 3000, 5000, 4.5, 5000, 8000, 8000, 50, 8000, 8000, 3000, 3000, 4.5, 3000, 6000, 4.5, 0.1, 3000, 4.5, 3000))
+      "speed" = c(30, 30, 50, 4.5, 50, 80, 80, 50, 80, 80, 30, 30, 4.5, 30, 60, 4.5, 0.1, 0.1))
+      #"speed" = c(3000, 3000, 5000, 4.5, 5000, 8000, 8000, 50, 8000, 8000, 3000, 3000, 4.5, 3000, 6000, 4.5, 0.1, 3000, 4.5, 3000))
 
     #   # convert speed to pace
     rSpd <- data.table::as.data.table(rSpd) %>%
