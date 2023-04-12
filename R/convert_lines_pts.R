1d595acd8b9d85d87de0a66bddead957c6ced2a1
gcperk <genevieve.c.perkins@gmail.com>
2023-04-12 14:56:32 -0700

    add function to create pts for incidental points in addition to lines

--- R/convert_lines_pts.R
+++ R/convert_lines_pts.R
@@ -1,6 +1,6 @@
-#' Convert Line segments to training points
+#' Convert Line segments or points to standardized training points
 #'
-#' @param processed_transects sf object of all attributed line segments
+#' @param processed_transects sf object of all attributed line segments or points
 #' @param trast raster template with the resolution
 #' @author Genevieve Perkins
 #' @return sf object points based on raster
@@ -9,28 +9,41 @@
 #' @import sf
 #' @importFrom terra rast extract vect
 #' @examples
-#' convert(lines, trast)
+#' convert_to_pts(lines, trast)
 #'
 
-convert_lines_pts <- function(processed_transects, trast) {
+convert_to_pts <- function(processed_transects, trast) {
+
+  #processed_transects <- inc_pts
 
   if ("ID" %in% colnames(processed_transects) == FALSE) {
     print ("adding ID column")
     processed_transects <- processed_transects %>%
-     dplyr::mutate(ID = seq(1, length(processed_transects$order), 1))
+      dplyr::mutate(ID = seq(1, length(processed_transects$order), 1))
   }
 
   processed_transects_id <- sf::st_drop_geometry(processed_transects)
 
-  # transv <- terra::vect(file.path(cleandat, "proc_s1_transects_datecreek.gpkg"))
+  geom_type <- as.character(unique(sf::st_geometry_type(processed_transects, by_geometry = TRUE)))
 
-  lBuff <- processed_transects %>%
-    sf::st_buffer(.,
-                  dist = 2.5,
-                  endCapStyle = "FLAT",
-                  joinStyle = "MITRE") %>%
-    sf::st_cast(., "MULTIPOLYGON")
+  if(geom_type %in% c("LINESTRING")) {
+    # for lines
+    lBuff <- processed_transects %>%
+      sf::st_buffer(.,
+                    dist = 2.5,
+                    endCapStyle = "FLAT",
+                    joinStyle = "MITRE") %>%
+      sf::st_cast(., "MULTIPOLYGON")
 
+
+  }  else {
+    # for points
+    lBuff <- processed_transects %>%
+      sf::st_buffer(.,
+                    dist = 2.5) %>%
+      sf::st_cast(., "MULTIPOLYGON")
+
+  }
   # convert to spatVect
   transv <- terra::vect(lBuff)
 
